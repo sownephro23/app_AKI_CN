a77c7b82afd63d3f8957203ff0b9f655a86369d4
sownephro23 <ibrahimaa.sow.75@gmail.com>
2025-06-14 18:21:39 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,30 @@
 const cacheName = 'nic-pwa-v1';
+// Chemins corrigés : on utilise des chemins relatifs (commençant par './')
 const filesToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => cache.addAll(filesToCache))
+      .then(cache => {
+        console.log('Mise en cache des fichiers pour le Service Worker');
+        return cache.addAll(filesToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Si la ressource est dans le cache, on la retourne.
+        // Sinon, on la récupère sur le réseau.
+        return response || fetch(event.request);
+      })
   );
 });
